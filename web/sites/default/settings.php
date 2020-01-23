0fe9c82a6a5c5a570f36d5c5dc8eb24fe2659f49
Andrés guerrero <aguerrero@weknowinc.com>
2020-01-23 16:20:29 -0500

    Adding new configurations for entity office

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,4 +4,5 @@
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+//TODO: Move to the profile dir
 $config_directories['sync'] = '../config/sync';
