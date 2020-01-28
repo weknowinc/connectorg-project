09090dd79fb6825fb5f526cfdf97cb1e9c1b0671
Andrés guerrero <aguerrero@weknowinc.com>
2020-01-28 13:56:44 -0500

    Adding module to handle all tools in the company

---
+++ web/modules/custom/owp_tool/src/Form/ToolEntityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\owp_tool\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Tool entities.
+ *
+ * @ingroup owp_tool
+ */
+class ToolEntityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
