f31547b25ef6c9b5771d4d240c1af8814bb05d4a
Jose Angel Bonfil <joseangelbonfil@gmail.com>
2020-06-09 15:23:01 -0500

    Feature/block birthday theme (#24)
    * git ignore change
    
    * Birthday block fix on query.
    
    * Birthday block fix on query.
    
    * Birthday block theming
    
    * Birthday block config
    
    * Birthday block adding icon.
    
    * Sending message in english with translation.
    
    Co-authored-by: Jose Angel Bonfil Evia <jbonfil@phase2technology.com>

--- web/modules/custom/connectorg_birthdays/src/Plugin/Block/TodaysBirthdays.php
+++ web/modules/custom/connectorg_birthdays/src/Plugin/Block/TodaysBirthdays.php
@@ -7,6 +7,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\user\Entity\User;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'TodaysBirthdays' block.
@@ -24,11 +25,12 @@ class TodaysBirthdays extends BlockBase
    */
   public function getAll()
   {
+    $config = $this->getConfiguration();
     $currentTime = new DrupalDateTime();
     $currentTime = $currentTime->format("m-d");
     $query = Drupal::database()->select('users', 'u')->fields('u');
     $query->innerJoin('user__field_birthday', 'field_birthday', 'field_birthday.entity_id = u.uid');
-    $query->innerJoin('user__field_avatar', 'field_avatar', 'field_avatar.entity_id = u.uid');
+    $query->innerJoin('user__user_picture', 'field_avatar', 'field_avatar.entity_id = u.uid');
     $expression = "DATE_FORMAT(field_birthday.field_birthday_value,'%m-%d')";
     $args = [
       ':birthday' => $currentTime
@@ -38,7 +40,7 @@ class TodaysBirthdays extends BlockBase
     $users = User::loadMultiple($ids);
     $listUsers = [];
     foreach ($users as $rowUser) {
-      $fileUri = $rowUser->get('field_avatar')->entity->getFileUri();
+      $fileUri =  ($rowUser->hasField('user_picture')) ? $rowUser->get('user_picture')->entity->getFileUri() : 'none.jpg';
       $id = $rowUser->id();
       $name = $rowUser->get('field_name')->value;
       $jobTitle = $rowUser->get('field_job_title')->entity->getName();
@@ -48,7 +50,9 @@ class TodaysBirthdays extends BlockBase
         'url' => $fileUri,
         'id' => $id,
         'name' => ucfirst($fullName),
-        'jobTitle' => ucfirst($jobTitle)
+        'jobTitle' => ucfirst($jobTitle),
+        'header' => $config['birthday_block_header'] ?: $this->t('Today is we celebrate the birthday of'),
+        'footer' => $config['birthday_block_footer'] ?: $this->t('Send a regard through Slack.'),
       ];
     }
     return ($listUsers);
@@ -57,6 +61,41 @@ class TodaysBirthdays extends BlockBase
   public function getCacheMaxAge()
   {
     return 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['birthday_block_header'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Header'),
+      '#description' => $this->t('Birthday block header message.'),
+      '#default_value' => isset($config['birthday_block_header']) ? $config['birthday_block_header'] : '',
+    ];
+
+    $form['birthday_block_footer'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Footer'),
+      '#description' => $this->t('Birthday block footer message.'),
+      '#default_value' => isset($config['birthday_block_footer']) ? $config['birthday_block_footer'] : '',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['birthday_block_header'] = $values['birthday_block_header'];
+    $this->configuration['birthday_block_footer'] = $values['birthday_block_footer'];
   }
 
   /**
